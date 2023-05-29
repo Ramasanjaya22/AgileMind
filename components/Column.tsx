9143ae0992e7ecf9081ead687e339281c2a6028c
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-05-29 19:27:39 +0700

    adding board

---
+++ components/Column.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Draggable, Droppable } from 'react-beautiful-dnd'
+
+type Props ={
+    id:TypedColumn;
+    todos: Todo[];
+    index: number;
+}
+
+const idToColumnText: {
+    [key in TypedColumn]: string;
+} = {
+    "todo" : "To Do",
+    "inprogress" : "In Progress",
+    "done" : "Done"
+}
+
+function Column({id,todos,index}:Props) {
+  return (
+    <Draggable
+    draggableId={id} index={index}
+    >
+        {(provided) =>(
+            <div
+            {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
+            >
+              <Droppable droppableId={index.toString()} type='card'>
+                {(provided,snapshot)=>(
+                    <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className={`p-2 rounded-2xl shadow-sm ${
+                        snapshot.isDraggingOver ? 'bg-green-200' : 'bg-white-50'
+                    }`}
+                    >
+                        <h2
+                        className='flex justify-between font-bold text-xl p-2'
+                        >{idToColumnText[id]}
+                        
+                        <span
+                        className='text-gray-500 bg-gray-200 px-2 py-1 text-sm rounded-full font-normal'
+                        >{todos.length}</span>
+                        </h2>
+                    </div>
+                )}
+              </Droppable>
+            </div>
+        )}
+    </Draggable>
+  )
+}
+
+export default Column
