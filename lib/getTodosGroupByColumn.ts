5da0bfd29564b4328c53b7494371d1b54c37412f
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-05-29 19:07:30 +0700

    grouping todos by columns using map

--- lib/getTodosGroupByColumn.ts
+++ lib/getTodosGroupByColumn.ts
@@ -5,5 +5,44 @@ export const getTodosGroupedByColumn = async() =>{
         process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!,
         process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID!,
     )
-    const 
+    const todos = data.documents
+
+    const columns = todos.reduce((acc,todo) =>{
+        if (!acc.get(todo.status)) {
+            acc.set(todo.status,{
+                id: todo.status,
+                todos: []
+            })
+        }
+
+        acc.get(todo.status)!.todos.push({
+            $id: todo.$id,
+            $createdAt: todo.$createdAt,
+            title: todo.title,
+            status: todo.status,
+            ...(todo.image && {image:JSON.parse(todo.image)})
+        });
+        return acc;
+    },new Map<TypedColumn,Column>)
+
+    // if columns doesnt have items, add with empty todos
+    const columnTypes: TypedColumn[] = ["todo","inprogress","done"];
+    for(const columnType of columnTypes){
+        if(!columns.get(columnType)){
+            columns.set(columnType,{
+                id: columnType,
+                todos: []
+            });
+        }
+    }
+    //sort columns by columnTypes
+    const sortedColumns = new Map(
+        Array.from(columns.entries()).sort((a,b)=>(
+            columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+        ))
+    )
+    const board: Board = {
+        columns: sortedColumns
+    }
+    return board
 }
